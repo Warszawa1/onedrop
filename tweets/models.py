f5583404dd42b6824896db2a503592ad2200cd61
Irene Antelo <iantelovazquez@gmail.com>
2023-02-10 00:29:52 +0100

    append tweets and reorder

--- tweets/models.py
+++ tweets/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Tweet(models.Model):
@@ -6,3 +7,15 @@ class Tweet(models.Model):
     # id = models.AutoField(primary_key=True)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+
+    class Meta:
+        ordering = ['-id']
+        
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "likes": random.randint(0, 200),
+            # "image": self.image.url
+        }

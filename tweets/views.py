b029bfe8b75d09954ba675e272b0a918a2de6a61
Irene Antelo <iantelovazquez@gmail.com>
2023-02-10 20:45:40 +0100

    render error message

--- tweets/views.py
+++ tweets/views.py
@@ -5,15 +5,13 @@ from django.utils.http import is_safe_url
 import random
 
 from .forms import TweetForm
-
 from .models import Tweet 
+
 
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
-# Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
-
 
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
@@ -28,6 +26,9 @@ def tweet_create_view(request, *args, **kwargs):
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
 
 

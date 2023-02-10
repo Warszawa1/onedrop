4b8609686b5940802bdc4998c1893427198ce8b5
Irene Antelo <iantelovazquez@gmail.com>
2023-02-10 21:08:02 +0100

    add superuser

--- tweets/models.py
+++ tweets/models.py
@@ -1,16 +1,22 @@
 from django.db import models
 import random
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 
 class Tweet(models.Model):
     # id is a hidden field that Django has 
     # id = models.AutoField(primary_key=True)
+    # many users can have one or many tweets but one tweet can have
+    # only one user
+    user = models.ForeignKey(User, on_delete=models.CASCADE)#once the user is gone all his stuff is too
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
     class Meta:
         ordering = ['-id']
-        
+
 
     def serialize(self):
         return {

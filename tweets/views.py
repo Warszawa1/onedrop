c6fceab0ff55a4c34d7c9c84cd03f82423451f3b
Irene Antelo <iantelovazquez@gmail.com>
2023-02-11 13:40:52 +0100

    set tweet delete filtering by ID

--- tweets/views.py
+++ tweets/views.py
@@ -3,7 +3,9 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.conf import settings
 from django.utils.http import is_safe_url
 
-from rest_framework.decorators import api_view
+from rest_framework.authentication import SessionAuthentication
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 import random
 
@@ -18,13 +20,14 @@ def home_view(request, *args, **kwargs):
     print(request.user or None)
     return render(request, "pages/home.html", context={}, status=200)
 
-
 '''
 REST API VIEW -> Django Rest Framework
 
 '''
 
 @api_view(['POST']) # http method the client == POST
+# @authentication_classes([SessionAuthentication, MyCustomAuth])
+@permission_classes([IsAuthenticated])
 def tweet_create_view(request, *args, **kwargs):
     serializer = TweetSerializer(data=request.POST)
     if serializer.is_valid(raise_exception=True):
@@ -41,6 +44,24 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     obj = qs.first()
     serializer = TweetSerializer(obj)
     return Response(serializer.data, status=200)
+
+
+
+@api_view(['DELETE', 'POST'])
+@permission_classes([IsAuthenticated])
+def tweet_delete_view(request, tweet_id, *args, **kwargs):
+    qs = Tweet.objects.filter(id=tweet_id)
+    if not qs.exists():
+        return Response({}, status=404)
+    qs = qs.filter(user=request.user)
+    if not qs.exists():
+        return Response({"message": "You cannot delete this tweet"}, status=401)
+    obj = qs.first()
+    obj.delete()
+    return Response({"message":"Tweet deleted"}, status=200)
+
+
+
 
 @api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
